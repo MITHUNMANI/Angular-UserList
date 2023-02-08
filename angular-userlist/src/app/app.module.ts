eda45c4277b68b9c67e1f6fabec3fd5b8fb2151c
Mithun <Mithun.Mani@partner.bmw.in>
2023-02-08 23:03:11 +0530

    fix : Basic setup on frontend / Backend

---
+++ angular-userlist/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserCreateComponent } from './components/user-create/user-create.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserCreateComponent,
+    UserEditComponent,
+    UserListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-userlist/src/app/components/employee-create/employee-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-create',
+  templateUrl: './employee-create.component.html',
+  styleUrls: ['./employee-create.component.css']
+})
+export class EmployeeCreateComponent {
+
+}
